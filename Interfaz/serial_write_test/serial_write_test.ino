1d927cdf73713b914411bb5d9824bebbe2959d64
GONZALO GRAU <ggrau@itba.edu.ar>
2024-08-14 17:21:09 -0300

    new serieal writer simulator

--- Interfaz/serial_write_test/serial_write_test.ino
+++ Interfaz/serial_write_test/serial_write_test.ino
@@ -1,13 +1,31 @@
+#include <math.h>
+#define BUFFER_SIZE 100
+
 hw_timer_t *timer = NULL;
 volatile SemaphoreHandle_t timerSemaphore;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
 
 int level = 1;
 int waveValue = 0;
 bool increasing = true;
 
+unsigned int buffer_read[BUFFER_SIZE];
+unsigned int buffer_send[BUFFER_SIZE];
+unsigned int sample_rate = 1e6;
+
+bool bufferFlag = false;
+bool sendFlag = false;
+static unsigned long counter = 0;
+static unsigned int pos = 0;
 
 void ARDUINO_ISR_ATTR onTimer() {
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter = isrCounter + 1;
+  lastIsrAt = millis();
+  portEXIT_CRITICAL_ISR(&timerMux);
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
 }
 
@@ -21,7 +39,7 @@ void setup() {
   timerSemaphore = xSemaphoreCreateBinary();
 
   // Set timer frequency to 1Mhz
-  timer = timerBegin(1000000);
+  timer = timerBegin(sample_rate);
 
   // Attach onTimer function to our timer.
   timerAttachInterrupt(timer, &onTimer);
@@ -32,26 +50,59 @@ void setup() {
 }
 
 void loop() {
-  // If Timer has fired
+  // If Timer has fired (this is basically the content of the onTimer call)
   if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE) {
-    
-    digitalWrite(LED_BUILTIN, level);
-      if (increasing) {
-        waveValue++;
-    if (waveValue >= 256) {
-      increasing = false;
+    // Every 500 ms, blink led
+    // Serial.println(lastIsrAt);
+    if (lastIsrAt % 500 == 0){
       level = !level;
       digitalWrite(LED_BUILTIN, level);
     }
-  } 
-  else {
-    waveValue--;
-    if (waveValue <= 0) {
-      increasing = true;
-    }
-  }
 
-  Serial.println(waveValue);
+    // Update wave value
+    if (increasing) {
+        waveValue++;
+      if (waveValue >= 256) {
+        increasing = false;
+      }
+    } 
+    else {
+      waveValue--;
+      if (waveValue <= 0) {
+        increasing = true;
+      }
+    }
+
+    // Write to buffer
+    buffer_read[pos] = waveValue;
+    pos++;
+    if (pos == BUFFER_SIZE){
+      for (unsigned int i = 0; i < BUFFER_SIZE; i++){
+        buffer_send[i] = buffer_read[i];
+      }
+      bufferFlag = true;
+      pos = 0;
+    }
+    counter++;
+
+  // Write to serial
+    if (Serial.available() > 0){
+        sendFlag = true;
+      }
+
+    if (bufferFlag && sendFlag){
+      char data = Serial.read();
+      if (data == 's'){ 
+        for (unsigned int i = 0; i < BUFFER_SIZE; i++){
+          int high = buffer_send[i] / 256;
+          // int low = buffer_send[i] % 256;
+          Serial.println(high);
+          // Serial.write(low);
+        }
+        bufferFlag = false;
+        sendFlag = false;
+      }
+    }
   }
 }
 
