90d3bc8458efec1c050d442f631319f010060032
Gonzalo Grau <ggrau@itba.edu.ar>
2024-09-10 17:26:00 -0300

    Further tasks paralellization

--- Interfaz/data_plot_test.py
+++ Interfaz/data_plot_test.py
@@ -1,13 +1,25 @@
 import sys
-import numpy as np
 from PyQt6 import QtWidgets, QtCore, QtSerialPort
-from PyQt6.QtGui import QWheelEvent
 import pyqtgraph as pg
 
 BAUD_RATE = 115200
-DATA_LEN = 1000
+DATA_LEN = 4000
 BUFFER_SIZE = 100
 REFRESH_RATE = 10  # Refresh rate in milliseconds
+
+class SerialReader(QtCore.QObject):
+    data_received = QtCore.pyqtSignal(bytes)
+
+    def __init__(self, serial_port):
+        super().__init__()
+        self.serial = serial_port
+
+    @QtCore.pyqtSlot()
+    def read_data(self):
+        if self.serial.canReadLine():
+            data = self.serial.read(BUFFER_SIZE)
+            if data:
+                self.data_received.emit(data)
 
 
 class SignalPlotter(pg.PlotWidget):
@@ -15,8 +27,7 @@ class SignalPlotter(pg.PlotWidget):
         super().__init__()
         self.setRange(xRange=(0, DATA_LEN), disableAutoRange=True)
 
-    def wheelEvent(self, event: QWheelEvent):
-        # Get the current x and y ranges
+    def wheelEvent(self, event):
         current_range = self.getViewBox().viewRange()
         old_range = current_range[0]
 
@@ -25,7 +36,8 @@ class SignalPlotter(pg.PlotWidget):
         factor = 1.1 if delta > 0 else 0.9
         new_right_limit = old_range[1] * factor
         new_x_range = (0, new_right_limit)
-        self.setRange(xRange=new_x_range)
+        if new_right_limit < DATA_LEN:
+            self.setRange(xRange=new_x_range)
         event.accept()
 
 
@@ -45,48 +57,71 @@ class Oscilloscope(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
 
-        # Set up the serial port connection
-        self.serial = QtSerialPort.QSerialPort()
-        self.serial.setBaudRate(BAUD_RATE)
-        self.serial.readyRead.connect(self.read_data)
-        self.setup_serial()
+        # Initialize data buffer
         self.data = [0 for _ in range(DATA_LEN)]
         self.data_len = DATA_LEN
 
-    def setup_serial(self):
+        # Set up the serial port in the main thread
+        port_name = self.get_serial_port()
+        if port_name:
+            self.serial = QtSerialPort.QSerialPort()
+            self.serial.setPortName(port_name)
+            self.serial.setBaudRate(BAUD_RATE)
+            if not self.serial.open(QtCore.QIODeviceBase.OpenModeFlag.ReadWrite):
+                QtWidgets.QMessageBox.critical(self, 'Serial Port Error', f"Can't open {port_name}")
+                self.close()
+                return
+
+            self.serial.write(b's')
+
+            # Set up the serial reader in a separate thread
+            self.serial_thread = QtCore.QThread()
+            self.reader = SerialReader(self.serial)
+            self.reader.moveToThread(self.serial_thread)
+            self.reader.data_received.connect(self.process_serial_data)
+            self.serial.readyRead.connect(self.reader.read_data)
+
+            # Start the thread
+            self.serial_thread.start()
+
+        else:
+            QtWidgets.QMessageBox.critical(self, 'Serial Port Error', 'No suitable serial port found.')
+            self.close()
+
+    @staticmethod
+    def get_serial_port():
         available_ports = QtSerialPort.QSerialPortInfo.availablePorts()
         for port in available_ports:
             if 'USB' in port.description():
-                self.serial.setPort(port)
-                if not self.serial.isOpen():
-                    if not self.serial.open(QtCore.QIODeviceBase.OpenModeFlag.ReadWrite):
-                        QtWidgets.QMessageBox.critical(self, 'Serial Port Error', f"Can't open {port.portName()}")
-                    else:
-                        print(f"Connected to {port.portName()}")
-                break
-        else:
-            QtWidgets.QMessageBox.critical(self, 'Serial Port Error', 'No suitable serial port found.')
-        self.serial.write(b's')
+                return port.portName()
+        return None
 
     def get_x_range(self):
         vb = self.plot_widget.getPlotItem().getViewBox()
         x_range = vb.viewRange()[0]  # First element is the x-axis range
         return x_range
 
-    def read_data(self):
-        if self.serial.isWritable():
-            self.serial.write(b's')
-
-        if self.serial.canReadLine():
-            data = self.serial.read(BUFFER_SIZE)
-            self.data.extend(data)
-            x_range = self.get_x_range()
-            self.data_len = int(x_range[1] - x_range[0]) + 1
-            if len(self.data) > self.data_len:
-                self.data = self.data[-self.data_len:]
+    @QtCore.pyqtSlot(bytes)
+    def process_serial_data(self, data):
+        self.data.extend(data)
+        x_range = self.get_x_range()
+        # self.data_len = int(x_range[1] - x_range[0]) + 1
+        if len(self.data) > self.data_len:
+            self.data = self.data[-self.data_len:]
 
     def update_plot(self):
+        self.serial.write(b's')
         self.plot.setData(self.data)
+
+    def closeEvent(self, event):
+        if hasattr(self, 'serial_thread') and self.serial_thread.isRunning():
+            self.serial_thread.quit()
+            self.serial_thread.wait()
+
+        if self.serial.isOpen():
+            self.serial.close()
+
+        event.accept()
 
 
 def main():
