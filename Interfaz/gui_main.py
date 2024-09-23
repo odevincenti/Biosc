3bf2386f90abc9165f28689d33ad6f064e456173
Gonzalo Grau <ggrau@itba.edu.ar>
2024-09-23 12:34:06 -0300

    further mockup design

--- Interfaz/gui_main.py
+++ Interfaz/gui_main.py
@@ -1,6 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6 import QtCore
 import pyqtgraph as pg
+import numpy as np
 
 uiclass, baseclass = pg.Qt.loadUiType("template.ui")
 
@@ -8,6 +10,10 @@ class MainWindow(uiclass, baseclass):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.widget.plot(np.random.normal(size=1000), pen=(255,0,0))
+        # Add a horizontal line at y=1 with dashed white pen
+        self.widget.addLine(y=1, pen=pg.mkPen('w', width=1, style=QtCore.Qt.DashLine))
+
 
 def main():
     app = QApplication(sys.argv)
