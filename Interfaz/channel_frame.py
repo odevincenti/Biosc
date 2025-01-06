48ee0e75ee7d8cabeb34512e1ffa1acb92b42133
Gonzalo Grau <i-gonzalo.grau@devrev.ai>
2025-01-06 12:33:36 -0300

    multichannel plotting

--- Interfaz/channel_frame.py
+++ Interfaz/channel_frame.py
@@ -8,7 +8,7 @@ class ChannelFrame(QFrame):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.formLayout = QFormLayout(self)
-        self.checkChannel = QCheckBox(self)
+        self.checkEnabled = QCheckBox(self)
         self.labelOffset = QLabel(self)
         self.spinOffset = QDoubleSpinBox(self)
         self.labelScale = QLabel(self)
@@ -20,36 +20,36 @@ class ChannelFrame(QFrame):
         return super().event(event)
 
     def setup_ui(self):
-        channel_name = self.property("channelName") or "Channel"
-        color = self.property("frameColor") or "rgb(246, 97, 81)"
+        self.channel_name = self.property("channelName") or "Channel"
+        self.color = self.property("frameColor") or "rgb(246, 97, 81)"
 
-        self.setObjectName(f"frame{channel_name}")
-        self.setStyleSheet(f"border-color: {color};")
+        self.setObjectName(f"frame{self.channel_name}")
+        self.setStyleSheet(f"border-color: {self.color};")
         self.setFrameShape(QFrame.Shape.NoFrame)
 
-        self.formLayout.setObjectName(f"formLayout_{channel_name}")
+        self.formLayout.setObjectName(f"formLayout_{self.channel_name}")
 
-        self.checkChannel.setObjectName(f"check{channel_name}")
-        self.checkChannel.setStyleSheet(f"background-color: {color};\ncolor: rgb(0, 0, 0);")
-        self.checkChannel.setText(channel_name)
-        self.formLayout.setWidget(0, QFormLayout.SpanningRole, self.checkChannel)
+        self.checkEnabled.setObjectName(f"check{self.channel_name}")
+        self.checkEnabled.setStyleSheet(f"background-color: {self.color};\ncolor: rgb(0, 0, 0);")
+        self.checkEnabled.setText(self.channel_name)
+        self.formLayout.setWidget(0, QFormLayout.SpanningRole, self.checkEnabled)
 
-        self.labelOffset.setObjectName(f"label{channel_name}offset")
+        self.labelOffset.setObjectName(f"label{self.channel_name}offset")
         self.labelOffset.setText("Offset:")
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.labelOffset)
 
-        self.spinOffset.setObjectName(f"spin{channel_name}offset")
+        self.spinOffset.setObjectName(f"spin{self.channel_name}offset")
         self.spinOffset.setMinimum(-5)
         self.spinOffset.setMaximum(+5)
         self.spinOffset.setSingleStep(0.05)
         self.spinOffset.setSuffix(" div")
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.spinOffset)
 
-        self.labelScale.setObjectName(f"label{channel_name}scale")
+        self.labelScale.setObjectName(f"label{self.channel_name}scale")
         self.labelScale.setText("Scale:")
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.labelScale)
 
-        self.spinScale.setObjectName(f"spinScale{channel_name}")
+        self.spinScale.setObjectName(f"spinScale{self.channel_name}")
         self.spinScale.setSuffix("mV")
         self.formLayout.setWidget(2, QFormLayout.FieldRole, self.spinScale)
 
--- Interfaz/gui_main.py
+++ Interfaz/gui_main.py
@@ -9,6 +9,13 @@ import numpy as np
 from comm_protocol import Commands
 from main_window_template import Ui_Bioscope
 from qt_serial_monitor import ESPSerial
+from typing import List, Tuple, Dict, TypedDict
+
+
+class SignalData(TypedDict):
+    channel: int
+    range: List[int]
+    signal: List[int]
 
 PROCESS_PATH = 'comm_protocol.py'
 SAMPLING_RATE = 10_000
@@ -24,24 +31,24 @@ class MainWindow(QMainWindow, Ui_Bioscope):
         self.esp_serial = ESPSerial()
         self.esp_serial.data_received.connect(self.handle_newdata)
 
-        # Plot utilities
-        self.pen_CH1 = mkPen(color=(255, 0, 0), width=2)
-
         # Dummy signals for testing
         self.dummy_signal = np.zeros(MAX_SIG_LEN)
         self.CH1_data = self.dummy_signal
-        self.update_plot()
 
         # Run button
         self.buttonRunStop.clicked.connect(self.on_click_push_run)
 
+        # Channel frames
+        self.channel_frames = [self.frameCH1, self.frameCH2, self.frameCH3, self.frameCH4]
+        self.channel_data = [self.dummy_signal, self.dummy_signal, self.dummy_signal, self.dummy_signal]
+        self.channel_pens = [mkPen(color=eval(frame.color.strip('rgb')), width=2) for frame in self.channel_frames]
+
         # Connect the spinbox to the plot
         self.frameCH1.spinScale.valueChanged.connect(self.update_plot)
 
-        # Connect the trigger button to the trigger line method from the plot widget
-        self.setup_capture_combo()
-        self.comboMode.currentTextChanged.connect(self.setup_capture_combo)
-        self.comboMode.setCurrentText('Trigger')
+    def channel_is_enabled(self, idx: int) -> bool:
+        # Use the checkbox to verify selection
+        return self.channel_frames[idx].checkEnabled.isChecked()
 
     ###################################################################################
     #               ESP32 Communication Methods                                       #
@@ -53,6 +60,16 @@ class MainWindow(QMainWindow, Ui_Bioscope):
         Start running
         """
         self.esp_serial.send_command(Commands.RUN)
+
+    @staticmethod
+    def parse_new_signal(signal_data: SignalData) -> np.ndarray:
+        """
+        Given signal data for channel, range, and samples, convert it to a numpy array and rescale
+        """
+        new_data = np.array(signal_data['signal']) / 255
+        new_range = np.array(signal_data['range']) * 1000
+        new_data = new_data * (new_range[1] - new_range[0]) + new_range[0]
+        return new_data
 
 
     @Slot(str)
@@ -70,23 +87,36 @@ class MainWindow(QMainWindow, Ui_Bioscope):
             return
 
         if message["command"] == Commands.MEASURED_SIGNALS:
-            new_data = np.array(message['data'][0]['signal']) / 255
-            new_range = np.array(message['data'][0]['range']) * 1000
-            new_data = new_data * (new_range[1] - new_range[0]) + new_range[0]
-            self.CH1_data = np.concatenate((self.CH1_data, new_data))
-            if len(self.CH1_data) > MAX_SIG_LEN:
-                self.CH1_data = self.CH1_data[-MAX_SIG_LEN:]
+            for i, signal_data in enumerate(message['data']):
+                channel_idx = signal_data['channel'] - 1 # 0 based indexing
+                assert 0 <= channel_idx < 4, f'Invalid channel index: {channel_idx}'
+                if not self.channel_is_enabled(channel_idx):
+                    continue
+
+                # Roll in new data. In the future, this should be handled by the capture mode
+                new_data = self.parse_new_signal(signal_data)
+                self.channel_data[channel_idx] = np.roll(self.channel_data[channel_idx], -len(new_data))
+                self.channel_data[channel_idx][-len(new_data):] = new_data
+
             self.update_plot()
 
     def update_plot(self):
-        scale = self.frameCH1.spinScale.value()
-        offset = self.frameCH1.spinOffset.value()
+        """
+        Currently, this function updates in rolling mode
+        In the future, it should handle the different capture modes
+        """
         self.plotWidget.clear()
         time_scale = self.plotWidget.getViewBox().viewRange()[0]
-        n_samples = int((time_scale[1] - time_scale[0]) * SAMPLING_RATE / 1_000_000) # in ms
+        n_samples = int((time_scale[1] - time_scale[0]) * SAMPLING_RATE / 1_000_000)  # in ms
         x_axis = np.linspace(time_scale[0], time_scale[1], n_samples)
-        plot_sig = (self.CH1_data[-len(x_axis):]) / scale + offset
-        self.plotWidget.plot(x_axis, plot_sig, pen=self.pen_CH1)
+
+        for i, frame in enumerate(self.channel_frames):
+            if not self.channel_is_enabled(i):
+                continue
+            scale = frame.spinScale.value()
+            offset = frame.spinOffset.value()
+            plot_sig = (self.channel_data[i][-len(x_axis):]) / scale + offset
+            self.plotWidget.plot(x_axis, plot_sig, pen=self.channel_pens[i])
 
     def closeEvent(self, event):
         """Handle window close event."""
